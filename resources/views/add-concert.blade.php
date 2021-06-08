6dc03ca773d8980047f52d8501a518229c1d1c90
Benjamin <r0720066@student.thomasmore.be>
2021-06-08 17:15:34 +0200

    Image preview

--- resources/views/add-concert.blade.php
+++ resources/views/add-concert.blade.php
@@ -2,9 +2,11 @@
 
 @section('title')
     Add concert
+  
 @endsection
 
 @section('content')
+ 
 <form method="post" action="/add-concert" enctype="multipart/form-data"> 
     @csrf
     <div class="container">
@@ -31,13 +33,16 @@
             <input type="range" name="price" class="range" min="5" max="10" step="1">
             <output class="bubble"></output>
         </div>
-        <h3 class="form-label h3-label">Picture</h3>
-        <div class="image-concert">    
-            <label class="custom-file-upload text-center">
-                <input type="file" name="photo"/>
-                <span class="material-icons">image</span>
-                <h5>Choose your image</h5>
-            </label>
+        <div>
+            <label for="photo" class="form-label h3-label inpFile">Add Image</label>
+        <!---    <input type="file" name="photo">-->
+        </div>
+        <input type="file" name="photo" id="inpFile">
+
+        <div class="image-preview" id="imagePreview">
+
+        <img src="" alt="Image Preview" class="image-preview__image">
+        <span class="image-preview__default-text"></span>
         </div>
     </div>
 @endsection
@@ -77,5 +82,34 @@
         const newVal = Number(((val - min) * 100) / (max - min));
         bubble.value = "€ " + val;
         }
+
+
+        const inpFile =document.getElementById("inpFile");
+        const previewContainer = document.getElementById("imagePreview");
+        const previewImage = previewContainer.querySelector(".image-preview__image");
+        const previewDefaultText =previewContainer.querySelector(".image-preview__default-tet");  
+
+        inpFile.addEventListener("change", function(){
+        const file =this.files[0];
+
+        if(file){
+        const reader = new FileReader(); //image lezen als url
+
+        previewImage.style.display ="block"; //geladen image tonen.
+       // previewDefaultText.style.display ="none"; //geladen image tonen.
+
+         reader.addEventListener("load", function(){      //toegevoegde img laden
+        previewImage.setAttribute("src", this.result);   //src veranderen in html, this verwijst nr fileReader
+
+         });
+
+         reader.readAsDataURL(file); //gelezen data tonen als image
+        }else{
+    previewImage.style.display = null; //als user niks kiest toon default css
+    previewDefaultText.Style.display =null;
+    previewImage.setAttribute("src","");
+}
+});
+
     </script>
 @endsection
